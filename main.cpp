d603a53c2687dc220b1580acabfefeba6668d49b
Daniel Luna <Circuitappstudios@gmail.com>
2018-12-08 14:09:06 -0500

    Line & Token Separator

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ int main() {
     std::string testASM = "mov raa, 5\n"
                           "mov rab, 5";
 
-    std::cout << testASM << std::endl;
-
     byteCode bCode = parseCode(testASM);
     //emualteBytecode(bCode, configuration);
 
@@ -19,7 +17,34 @@ int main() {
 }
 
 std::vector<unsigned char> parseCode(std::string code){
-    std::cout << "Parsing Code" << std::endl;
+    std::cout << "Parsing Code\n" << std::endl;
 
+    std::vector <std::string> lines;
+    std::vector <std::string> tokens;
+    std::stringstream codeStream(code);
+    std::string intermediate;
+
+    while (std::getline(codeStream, intermediate, '\n')){
+        lines.push_back(intermediate);
+    }
+
+    std::cout << "Printing Lines" << std::endl;
+    std::cout << "--------------\n" << std::endl;
+
+    for(int i = 0; lines.size() > i; i++){
+        std::cout << "Line " << i + 1 << ". " << lines[i] << std::endl;
+        std::stringstream lineStream(lines[i]);
+
+        while(std::getline(lineStream, intermediate, ' ')){
+            tokens.push_back(intermediate);
+        }
+    }
+
+    std::cout << "\nPrinting Tokens" << std::endl;
+    std::cout << "---------------\n" << std::endl;
+
+    for(int i = 0; tokens.size() > i; i++){
+        std::cout << tokens[i] << std::endl;
+    }
 
 }
